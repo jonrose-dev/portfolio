501f620a10e8ab9b2e6a21aac4a7acbede139243
Jon <jon@jonrose.dev>
2021-01-30 15:59:14 -0500

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+const readyFunc = _ => {
+    fetch('https://arcane-bastion-52844.herokuapp.com/notify')
+    .then(res => console.log(res))
+    .catch(e => console.error(e));
+    Array.from(document.getElementsByClassName('scroll')).forEach(elem => {
+        elem.addEventListener('click', event => {
+            event.preventDefault();
+            const id = elem.getAttribute('href').split('#')[1];
+            document.getElementById(id).scrollIntoView({behavior: 'smooth' })
+
+        });
+    })
+};
+
+document.addEventListener('DOMContentLoaded', readyFunc, false);
+  
